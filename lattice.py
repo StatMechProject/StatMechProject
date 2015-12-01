1df126a0ca6687da549c0569554e4da456ba1ed9
wczuniga <wzuniga@hmc.edu>
2015-12-01 00:18:13 -0800

    Drafted Produced, things still not working

--- lattice.py
+++ lattice.py
@@ -7,9 +7,10 @@ boundaryMask = np.zeros((512,256))
 X,Y  = np.mgrid[0:512,0:256]
 #boundaryMask[0,:]=boundaryMask[-1,:]=
 boundaryMask[:,0]=boundaryMask[:,-1]=1
-circleMask = ((256-X)**2 + (128-Y)**2) < 25**2
-boundaryMask[circleMask] = 1
+#circleMask = ((256-X)**2 + (128-Y)**2) < 25**2
+#boundaryMask[circleMask] = 1
 
+dividByZeroFudgeFactor = 1E-15
 
 
 class Lattice:
@@ -17,8 +18,9 @@ class Lattice:
     def __init__(self,reflectMesh=boundaryMask,Nx=512,Ny=256):
     	self.mew = .00001857 # constant for the mu
         self.Nx,self.Ny,self.Nvecs = 512,256,9
-        self.dx=.001; self.dt=.01
-        self.c = self.dx/self.dt
+        self.dx=.001
+        self.c = 340
+        self.dt = self.dx/self.c
         self.Fi = np.zeros((self.Nvecs,Nx,Ny)); 
         self.FiStar = self.Fi
         self.FiEq = self.Fi
@@ -28,14 +30,17 @@ class Lattice:
         self.es = np.array([0,1,1j,-1,-1j,1+1j,-1+1j,-1-1j,1-1j]).reshape((9,1,1))
         self.ws = np.array([4/9.,1/9.,1/9.,1/9.,1/9.,1/36.,1/36.,1/36.,1/36.]).reshape((9,1,1))
         self.reflectMesh = reflectMesh.astype(bool)
-        self.inletVelocity = 25
+        self.inletVelocity = 70
+        self.whereFluid = (boundaryMask==0)
         #self.updateRhoAndU()
         self.initFs()
+
         
 
     def initFs(self):
         # using density of air as 1.225 kg/m3 so over 1/800g per mm2
-        self.Fi = np.ones((self.Nvecs,self.Nx,self.Ny))/800.
+
+        self.Fi[0] = np.ones((self.Nx,self.Ny)) * 1.225 * self.dx**2 * self.whereFluid
         self.stream()
         self.updateRhoAndU()
 
@@ -57,7 +62,7 @@ class Lattice:
         # Reflects at the 1's on the mesh
         xShift,yShift = np.real(es),np.imag(es)
         revDirI = np.array([0,3,4,1,2,7,8,5,6])
-        for i in np.arange(1,Nvecs,2):
+        for i in [1,2,5,6]:
             swapCopy = np.copy(FiStar[i,reflectMask])
             FiStar[i,reflectMask] = FiStar[revDirI[i],reflectMask]
             FiStar[revDirI[i],reflectMask] = swapCopy
@@ -65,7 +70,8 @@ class Lattice:
     def updateRhoAndU(self):
         es,c,FiStar = self.es,self.c,self.FiStar
         self.rho = np.sum(FiStar,axis=0)
-        self.u = (1./self.rho)*np.sum(FiStar*es,axis=0)*c
+        sumEFi = np.sum(FiStar*es,axis=0)
+        self.u= c*sumEFi/(self.rho+dividByZeroFudgeFactor)
 
 
 
@@ -81,39 +87,25 @@ class Lattice:
     def inletOutlet(self):
         FiStar = self.FiStar
         # inlet
-        self.rho[0,:] = (FiStar[0,0,:]+FiStar[2,0,:]+FiStar[4,0,:]+\
-                   2*(FiStar[3,0,:]+FiStar[6,0,:]+FiStar[7,0,:]))/(1-self.inletVelocity)
-        FiStar[1,0,:] = FiStar[3,0,:]
-        FiStar[5,0,:] = FiStar[7,0,:] - 1/2 * (FiStar[2,0,:] - FiStar[4,0,:]) + 1/6 * self.rho[0,:] * self.inletVelocity
-        FiStar[8,0,:] = FiStar[6,0,:] - 1/2 * (FiStar[2,0,:] - FiStar[4,0,:]) + 1/6 * self.rho[0,:] * self.inletVelocity
+        self.rho[0,1:-1] = (FiStar[0,0,1:-1]+FiStar[2,0,1:-1]+FiStar[4,0,1:-1]+\
+                   2*(FiStar[3,0,1:-1]+FiStar[6,0,1:-1]+FiStar[7,0,1:-1]))/(1-self.inletVelocity)
+        FiStar[1,0,1:-1] = FiStar[3,0,1:-1]
+        FiStar[5,0,1:-1] = FiStar[7,0,1:-1] - 1/2 * (FiStar[2,0,1:-1] - FiStar[4,0,1:-1]) + 1/6 * self.rho[0,1:-1] * self.inletVelocity
+        FiStar[8,0,1:-1] = FiStar[6,0,1:-1] - 1/2 * (FiStar[2,0,1:-1] - FiStar[4,0,1:-1]) + 1/6 * self.rho[0,1:-1] * self.inletVelocity
         
         # outlet
         # done by swapping respective 3-1, 6-5, 7-8 for all following code
-        self.rho[-1,:] = (FiStar[0,0,:]+FiStar[2,0,:]+FiStar[4,0,:]+\
-                    2*(FiStar[1,0,:]+FiStar[5,0,:]+FiStar[8,0,:]))/(1-self.inletVelocity)
-        FiStar[3,0,:] = FiStar[1,0,:]
-        FiStar[6,0,:] = FiStar[5,0,:] - 1/2 * (FiStar[2,0,:] - FiStar[4,0,:]) + 1/6 * self.rho[0,:] * self.inletVelocity
-        FiStar[7,0,:] = FiStar[8,0,:] - 1/2 * (FiStar[2,0,:] - FiStar[4,0,:]) + 1/6 * self.rho[0,:] * self.inletVelocity
+        self.rho[-1,1:-1] = (FiStar[0,0,1:-1]+FiStar[2,0,1:-1]+FiStar[4,0,1:-1]+\
+                    2*(FiStar[1,0,1:-1]+FiStar[5,0,1:-1]+FiStar[8,0,1:-1]))/(1-self.inletVelocity)
+        FiStar[3,-1,1:-1] = FiStar[1,-1,1:-1]
+        FiStar[6,-1,1:-1] = FiStar[5,-1,1:-1] - 1/2 * (FiStar[2,-1,1:-1] - FiStar[4,-1,1:-1]) + 1/6 * self.rho[-1,1:-1] * self.inletVelocity
+        FiStar[7,-1,1:-1] = FiStar[8,-1,1:-1] - 1/2 * (FiStar[2,-1,1:-1] - FiStar[4,-1,1:-1]) + 1/6 * self.rho[-1,1:-1] * self.inletVelocity
 
 
-
-
-    # Depreciated, replaced by reflectOnMesh
-    def reflectOnBorder(self): 
-        es,FiStar,Nvecs = self.es,self.FiStar,self.Nvecs
-        #Boundary Reflection
-        xShift,yShift = np.real(es),np.imag(es) #Maps 1=>0 and -1=>-1 for indexes
-        xSIndex = (.5*xShift-.5).astype(int); ySIndex = (.5*yShift-.5).astype(int); 
-        revDirI = np.array([0,3,4,1,2,7,8,5,6])
-        for i in np.arange(Nvecs): 
-            if xShift[i]:
-                FiStar[i,xSIndex[i],:]=FiStar[revDirI[i],xSIndex[revDirI[i]],:]
-            if yShift[i]:
-                FiStar[i,:,ySIndex[i]]=FiStar[revDirI[i],:,ySIndex[revDirI[i]]]
 
     def updateFi(self):
     	# Update the the relaxation time constant
-    	viscosity = self.mew/self.rho # 2d array type
+    	viscosity = self.mew/(self.rho+dividByZeroFudgeFactor) # 2d array type
     	tau = ((viscosity*6*self.dt/(self.dx)**2)+1)/2 ## also arraytype
     	# update Fi from FiStar using the collision operator
         conj_u = np.conj(self.u) 
