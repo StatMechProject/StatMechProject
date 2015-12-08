ef2668057805c119079831e7ecb02bdc99ff3721
wczuniga <wzuniga@hmc.edu>
2015-12-07 21:12:08 -0800

    Update lattice.py
    from my laptop

--- lattice.py
+++ lattice.py
@@ -2,52 +2,58 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
-# Make the recutangular boundary mask
-boundaryMask = np.zeros((512,256))
-X,Y  = np.mgrid[0:512,0:256]
-#boundaryMask[0,:]=boundaryMask[-1,:]=
-boundaryMask[:,0]=boundaryMask[:,-1]=1
-circleMask = ((256-X)**2 + (128-Y)**2) < 25**2
-boundaryMask[circleMask] = 1
 
-dividByZeroFudgeFactor = 1E-10
+defaultX = 100
+defaultY = 100
+# Make the recutangular boundary mask
+boundaryMask = np.zeros((defaultX,defaultY))
+X,Y  = np.mgrid[0:defaultX,0:defaultY]
+testMask =  (X < 53) * (X > 47) * (Y < 70) * (Y > 30)
+boundaryMask[testMask] = 1
+#circleMask = ((defaultX/2-X)**2 + (defaultY/2-Y)**2) < 25**2
+#boundaryMask[circleMask] = 1
+
+dividByZeroFudgeFactor = 1E-15
 
 
 class Lattice:
     
-    def __init__(self,reflectMesh=boundaryMask,Nx=512,Ny=256):
-    	self.mew = .00001857 # constant for the mu
-        self.Nx,self.Ny,self.Nvecs = 512,256,9
-        self.dx=.001
-        self.c = 340.
+    def __init__(self,reflectMesh=boundaryMask,Nx=defaultX,Ny=defaultY):
+    	self.mu = .00001857 # constant for the mu
+        self.Nx,self.Ny,self.Nvecs = Nx,Ny,9
+        self.dx=.001 # 1mm spacing
+        self.c = 340. # speed of sound 340 m/s
         self.dt = self.dx/self.c
         self.Fi = np.zeros((self.Nvecs,Nx,Ny)); 
         self.FiStar = self.Fi
         self.FiEq = self.Fi
         self.rho = np.zeros((Nx,Ny))
-        self.u = np.zeros((Nx,Ny))
+        self.u = np.zeros((Nx,Ny)) # 2d grid with complex numbers for vel vectors
         self.s = self.Fi
-        self.es = np.array([0,1,1j,-1,-1j,1+1j,-1+1j,-1-1j,1-1j]).reshape((9,1,1))
-        self.ws = np.array([4/9.,1/9.,1/9.,1/9.,1/9.,1/36.,1/36.,1/36.,1/36.]).reshape((9,1,1))
-        self.reflectMesh = reflectMesh.astype(bool)
-        self.inletVelocity = 70.
-        self.whereFluid = (boundaryMask==0)
-        #self.updateRhoAndU()
+        self.es = np.array([0,1,1j,-1,-1j,1+1j,-1+1j,-1-1j,1-1j]).reshape((9,1,1)) #Our velocity basis
+        self.ws = np.array([4/9.,1/9.,1/9.,1/9.,1/9.,1/36.,1/36.,1/36.,1/36.]).reshape((9,1,1)) #u weight
+        self.reflectMesh = reflectMesh.astype(bool) # Mask containing borders an objects in our grid
+        self.inletVelocity = 70. #m/s
+        self.rho0 = 1.184 * self.dx**2 
+        self.whereFluid = (boundaryMask==0) # All the places where we should be able to have fluid
         self.initFs()
 
         
 
     def initFs(self):
-        # using density of air as 1.184 kg/m3 so over 1/800g per mm2
+        # using density of air as 1.184 kg/m3 and our grid is 1m thick
+        # Uniformly distribute over velocity and position space, remove fluid in boundaries
 
-        self.Fi = np.ones((self.Nvecs,self.Nx,self.Ny)) * 1.184 * self.dx**2 * self.whereFluid/9.
-        self.stream()
-        self.updateRhoAndU()
+        self.Fi = np.ones((self.Nvecs,self.Nx,self.Ny)) * self.rho0 * self.whereFluid/9.
+        es,c,Fi = self.es,self.c,self.Fi
+        self.rho = np.sum(Fi,axis=0)
+        sumEFi = np.sum(Fi*es,axis=0)
+        self.u= c*sumEFi/(self.rho+dividByZeroFudgeFactor)*self.whereFluid
 
         
     def stream(self):
         es,Nvecs = self.es,self.Nvecs
-        #Streaming step
+        #Streaming step, move each of the distributions in their direction
         xShift,yShift = np.real(es).astype(int),np.imag(es).astype(int)
         for i in np.arange(Nvecs):
             self.FiStar[i] = self.Fi[i]
@@ -85,14 +91,14 @@ class Lattice:
 
 
     def inletOutlet(self):
-        FiStar = self.FiStar
+        FiStar = self.FiStar # Applies Zou Hue boundary conditions on top and bottom
         # inlet
         self.rho[0,1:-1] = (FiStar[0,0,1:-1]+FiStar[2,0,1:-1]+FiStar[4,0,1:-1]+\
                    2*(FiStar[3,0,1:-1]+FiStar[6,0,1:-1]+FiStar[7,0,1:-1]))/(1-self.inletVelocity)
         FiStar[1,0,1:-1] = FiStar[3,0,1:-1]
         FiStar[5,0,1:-1] = FiStar[7,0,1:-1] - 1/2 * (FiStar[2,0,1:-1] - FiStar[4,0,1:-1]) + 1/6 * self.rho[0,1:-1] * self.inletVelocity
         FiStar[8,0,1:-1] = FiStar[6,0,1:-1] - 1/2 * (FiStar[2,0,1:-1] - FiStar[4,0,1:-1]) + 1/6 * self.rho[0,1:-1] * self.inletVelocity
-        
+     
         # outlet
         # done by swapping respective 3-1, 6-5, 7-8 for all following code
         self.rho[-1,1:-1] = (FiStar[0,0,1:-1]+FiStar[2,0,1:-1]+FiStar[4,0,1:-1]+\
@@ -105,7 +111,7 @@ class Lattice:
 
     def updateFi(self):
     	# Update the the relaxation time constant
-    	viscosity = self.mew/(self.rho+dividByZeroFudgeFactor) # 2d array type
+    	viscosity = self.mu/(self.rho+dividByZeroFudgeFactor) # 2d array type
     	tau = ((viscosity*6*self.dt/(self.dx)**2)+1)/2 ## also arraytype
     	# update Fi from FiStar using the collision operator
         conj_u = np.conj(self.u) 
@@ -115,15 +121,16 @@ class Lattice:
         self.Fi = self.Fi - 1/tau * (self.FiStar - self.FiEq) * self.whereFluid
 
     def fullTimeStep(self):
-        self.stream()
-        self.inletOutlet()
-        self.reflectOnMesh()
-        self.updateRhoAndU()
-        self.updateFi()
+        self.stream() # Move distributions disregarding collision
+        self.inletOutlet() # Adjust inlet and outlet distrubutions
+        self.reflectOnMesh() # Reverse velocities of distributions that are out of bounds
+        self.updateRhoAndU() # Calculate macroscopic quantities
+        
+        self.updateFi() # Apply collision operator to determine proper Fi from FiEq
 
 def plttt(latt):
     #X,Y = np.mgrid[0:.511:512j,0:.255:256j]
-    X,Y = np.linspace(0,.511,512),np.linspace(0,.255,256)
+    X,Y = np.linspace(0,(self.dx * (defaultX - 1)),defaultX),np.linspace(0,(self.dx * (defaultY - 1)),defaultY)
     ux,uy =np.real(latt.u),np.imag(latt.u)
     fig = plt.figure()
     print np.shape(X),np.shape(Y),np.shape(ux),np.shape(uy)
@@ -140,4 +147,3 @@ class Visualization:
     
     def animate(self):
         return
-
