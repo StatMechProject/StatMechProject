353b7104d0cb71fba64b51d0ca51b50381b3aca1
AlecRBGriffith <agriffith@hmc.edu>
2015-12-10 19:57:21 -0800

    removed plotting so could be run seperately, plotting in plot_data

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 import lattice
-import matplotlib.pyplot as plt
 import numpy as np
+
+simType = raw_input('Please give the file name for the run: ')
 x = lattice.Lattice()
 
-for i in range(1000000):
-	x.fullTimeStep()
-	if i % 200 == 0:
-		fig = plt.imshow(np.real(x.u).T)
-  		plt.title('Cylinder X-Direction Velocity', size = 14)
-		cbar = plt.colorbar()
-  		cbar.set_label('m/s',size = 12)
-  		plt.axis('off')
-		plt.savefig('test.pdf', bbox_inches='tight')
-		plt.show()
-		print x.convergence()
+for i in range(10000):
+    x.fullTimeStep()
+    
+    if i % 10 == 0:
+        print x.convergence()
+        np.savetxt(simType + '_ux.csv', np.real(x.u))
+        np.savetxt(simType + '_uy.csv', np.imag(x.u))
+        np.savetxt(simType + '_rho.csv', x.rho)
+        
