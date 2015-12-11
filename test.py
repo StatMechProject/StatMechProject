48377eb029b0a2adbf29869cde1ac4764f878514
AlecRBGriffith <agriffith@hmc.edu>
2015-12-10 16:33:33 -0800

    improved plotting

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
-import Lettuce
+import lattice
 import matplotlib.pyplot as plt
 import numpy as np
-x = Lettuce.Lattice()
+x = lattice.Lattice()
 
 for i in range(1000000):
 	x.fullTimeStep()
-	if i % 5000 == 0:
-		plt.imshow(np.abs(x.u))
-		plt.colorbar()
+	if i % 200 == 0:
+		fig = plt.imshow(np.real(x.u).T)
+  		plt.title('Cylinder X-Direction Velocity', size = 14)
+		cbar = plt.colorbar()
+  		cbar.set_label('m/s',size = 12)
+  		plt.axis('off')
+		plt.savefig('test.pdf', bbox_inches='tight')
 		plt.show()
-
+		print x.convergence()
